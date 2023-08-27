1e564aeb9692429179d376774612a2a9dc95be61
Tudyx <56633664+Tudyx@users.noreply.github.com>
2023-08-27 23:08:12 +0200

    initial config

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+# Empty initial greeting.
+set fish_greeting
+# Full directory on fish prompt.
+set fish_prompt_pwd_dir_length 0
