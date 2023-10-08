70ccfe1c7e3d7144c161d4ea6cde2b3943380533
Tudyx <56633664+Tudyx@users.noreply.github.com>
2023-10-08 14:30:50 +0200

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
+abbr -a c 'cargo'
+abbr -a g 'git'
+abbr -a ga 'git add -p'
+abbr -a ip 'ip --color=auto'
+
+# Alias
+abbr -a cat bat
 if command -v exa > /dev/null
 	abbr -a l 'exa'
 	abbr -a ls 'exa'
@@ -13,3 +20,7 @@ end
 set fish_greeting
 # Full directory on fish prompt.
 set fish_prompt_pwd_dir_length 0
+
+setenv EDITOR hx
+# TODO: add hx to the path also for the root user.
+# setenv SUDO_EDITOR hx
