63803aa1e2cf37d34a841f0946746a31a734cdb1
Tudyx <56633664+Tudyx@users.noreply.github.com>
2023-12-01 01:17:44 +0100

    tmux style conf

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -19,15 +19,16 @@ end
 config.color_scheme = 'Google (dark) (terminal.sexy)'
 -- Default shell
 config.default_prog= {'/usr/bin/fish'}
--- No padding around window
 config.window_padding = {
-  left = 0,
+  left = 2,
   right = 0,
   top = 0,
   bottom = 0,
 }
--- Replace fullscreen key bindings as there is a conflic with helix.
+config.enable_scroll_bar = true
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
+  -- Replace fullscreen key bindings as there is a conflic with helix.
   {
     key = 'Enter',
     mods = 'ALT',
@@ -36,8 +37,65 @@ config.keys = {
   {
     key = 'F11',
     action = wezterm.action.ToggleFullScreen
-  }
-}
+  },
+  -- Reproduce tmux shorcut
+  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+  -- This resolve the conflicts with emacs "go to begin of the line" used in most of the shell
+  {
+    key = 'a',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateLastTab,
+  },
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    key = 'n',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = '\\',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical{ domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'c',
+    mods = 'LEADER',
+    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+  },
+    { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
+    { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
+    { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
+    { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+    { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+    { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+    { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+    { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+    { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
+    { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
+    { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
+    { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
+    { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
+    { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
+    { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
+    { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
+    { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+    -- { key = "o", mods = "LEADER",       action="TogglePaneZoomState" },
+    -- { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
+ }
 
 -- and finally, return the configuration to wezterm
 return config
