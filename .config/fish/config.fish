a8e4a78e6627d9e0c61a623eb7e69f6640399cb8
Tudyx <56633664+Tudyx@users.noreply.github.com>
2025-03-08 20:00:21 +0100

    try out starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,11 +16,6 @@ else
     abbr -a lll 'ls -la'
 end
 
-# Empty initial greeting.
-set fish_greeting
-# Full directory on fish prompt.
-set fish_prompt_pwd_dir_length 0
-
 setenv EDITOR hx
 # hx is not in the path for the root user.
 setenv SUDO_EDITOR $(which hx)
@@ -34,3 +29,5 @@ setenv LESS_TERMCAP_se \e'[0m' # end standout-mode
 setenv LESS_TERMCAP_so \e'[38;5;246m' # begin standout-mode - info box
 setenv LESS_TERMCAP_ue \e'[0m' # end underline
 setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+source (/usr/local/bin/starship init fish --print-full-init | psub)
